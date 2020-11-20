4189c84b349cc512102f3b68aff5edd69d93215f
Pavan Manikanta Kiran Maddu <m.p.m.kiran@gmail.com>
2020-11-20 10:48:35 +0530

    initial commit

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ const port = process.env.PORT || 5000
 
 
 const mysql = require("mysql2/promise");
-/*
+
 const pool = mysql.createPool({
     host: process.env.DB_URL || "localhost",
-    port:  3306,
+    port: 3306,
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "root",
-    database: process.env.DB_NAME || "training_db",
+    database: process.env.DB_NAME || "todoapp_db",
     connectionLimit: 1
 });
-*/
-app.get("/", (req,res)=> res.send({message:"REST API Service is working"}))
 
-app.listen(port, () => console.log(`Example app listening on port!`, port ))
+app.get("/", (req, res) => res.send({ message: "REST API Service is working" }));
+
+app.listen(port, () => console.log(`Example app listening on port!`, port));
